a20ec9db97d27443b95b259d1c33b1ce7d39c96c
Patience Shyu <patienceshyu@gmail.com>
2021-12-24 10:52:45 +0100

    Improve styling

--- components/container.js
+++ components/container.js
@@ -1,3 +1,3 @@
 export default function Container({ children }) {
-  return <div className="container mx-auto w-3/4 intro-card jumbotron">{children}</div>
+  return <div className="container mx-auto w-11/12 sm:w-5/6 intro-card jumbotron">{children}</div>
 }
--- components/layout.js
+++ components/layout.js
@@ -4,10 +4,8 @@ export default function Layout({ preview, children }) {
   return (
     <>
       <Meta />
-      <div>
-        <div className="intro">
-          <main>{children}</main>
-        </div>
+      <div className="intro">
+        <main>{children}</main>
       </div>
     </>
   )
